92b9b9db0ca7912a8308b049391190ae6c5920ab
@cyruskiprotich8 <cyruskiprotich8@gmail.com>
2024-01-27 16:45:36 +0300

    truth testing depemdency

---
+++ app/src/androidTest/java/com/example/weatherwise/data/repository/WeatherRepositoryImplTest.kt
@@ -0,0 +1,24 @@
+package com.example.weatherwise.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class WeatherRepositoryImplTest {
+
+    @Test
+    fun getCurrentWeather() {
+    }
+
+    @Test
+    fun getForecastWeather() {
+    }
+
+    @Test
+    fun updateForecastWeatherFavoriteStatus() {
+    }
+
+    @Test
+    fun insertFavouritePlace() {
+    }
+}
